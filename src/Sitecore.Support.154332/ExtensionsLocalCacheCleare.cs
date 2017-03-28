257acba568095f704f9c769b862119cf6a1ed18f
MasoudA <masoud_nitro@yahoo.com>
2017-03-28 17:50:42 +0800

    Initial Release

---
+++ src/Sitecore.Support.154332/ExtensionsLocalCacheCleare.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Analytics.Data.Items;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Sitecore.Support
+{
+    public class ExtensionsLocalCacheCleare
+    {
+        /// <summary>Clears the cache.</summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="args">The arguments.</param>
+        public void ClearItemExtensionLocalCache(object sender, EventArgs args)
+        {
+            Assert.ArgumentNotNull(sender, "sender");
+            Assert.ArgumentNotNull((object)args, "args");
+            //Extensions.RemoveCacheEntry(item.Database.Name);
+            //var fldInfo = typeof(Sitecore.Analytics.Data.Items.Extensions).GetMethod("RemoveCacheEntry", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.IgnoreCase);
+            // fldInfo.Invoke(this, new object[] { "{web}" });
+
+            var fldInfo = typeof(Sitecore.Analytics.Data.Items.Extensions).GetField("localCache", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.IgnoreCase);
+            var currentValue = fldInfo.GetValue(null) as List<AnalyticsItems>;
+            currentValue.Clear();
+        }
+    }
+}
---
+++ src/Sitecore.Support.154332/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.154332")]
+[assembly: AssemblyProduct("Sitecore.Support.154332")]
+[assembly: ComVisible(false)]
